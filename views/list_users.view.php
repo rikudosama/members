be8c212d6cee14f4ec03d1a657019ba85ceb876c
rikudosama <bjean633@gmail.com>
2017-01-26 11:57:33 +0000

    a bit style of members' liste

--- views/list_users.view.php
+++ views/list_users.view.php
@@ -3,21 +3,24 @@
 
 <div id="main-content">
     <div class="container">
-     
-     <h1>Liste des membres</h1>
 
-     <?php foreach ($members as $member): ?>
-         <div class="member-block">
+     <h1>Liste des membres</h1>
+  <?php foreach (array_chunk($members, 4) as $member_set): ?>
+    <div class="row members">
+     <?php foreach ($member_set as $member): ?>
+       <div class="col-md-3 member-block">
+         <a href="profile.php?id=<?=$member->id?>">
+           <img src="<?= $member->avatar ? $member->avatar : get_avatar_url(e($member->email)) ?>" alt="<?=e($member->pseudo)?>" class="avatar img-polaroid">
+         </a>
+         <h4 class="member-block-membername">
            <a href="profile.php?id=<?=$member->id?>">
-             <img src="<?= $member->avatar ? $member->avatar : get_avatar_url(e($member->email)) ?>" alt="<?=e($member->pseudo)?>" class="img-polaroid avatar-md">
+             <?=e($member->pseudo)?>
            </a>
-           <h4 class="member-block-membername">
-             <a href="profile.php?id=<?=$member->id?>">
-               <?=e($member->pseudo)?>
-             </a>
-           </h4>
-         </div>
-     <?php endforeach?>
+         </h4>
+       </div>
+     <?php endforeach;?>
+    </div>
+  <?php endforeach;?>
     </div>
 </div>
 
