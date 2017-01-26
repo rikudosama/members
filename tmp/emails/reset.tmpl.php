db6d112a06995ab0ab38655cdd5c112eb0be3405
rikudosama <bjean633@gmail.com>
2017-01-26 12:48:33 +0000

    reset password template

---
+++ tmp/emails/reset.tmpl.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+     <head>
+      <meta charset="UTF-8">
+     </head>
+     <body>
+      <h1>Réinitialisation de votre mot de passe</h1>
+      pour réinitialiser votre mot de passe veillez cliquer sur ce lien
+      <a href="<?=WEBESITE_URL.'/reset.php?p='.$pseudo.'&amp;token='.$token ?>">liens d'activation</a>
+     </body>
+</html>
